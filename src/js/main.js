5a0cb94c58759a872b04e311d55be9db5c42ded8
Vasyl Bats <basebv@gmail.com>
2017-02-28 16:42:37 +0200

    10th line, unfinished 2nd slider

--- src/js/main.js
+++ src/js/main.js
@@ -27,4 +27,29 @@ $(document).ready(function () {
             }
         ]
     });
+    $('.center').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 });
